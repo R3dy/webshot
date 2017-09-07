e0ecf6047fce1a5e3551d439a3b386ab6cdaecec
royce.davis <royce.davis@servicenow.com>
2017-09-07 12:35:30 -0500

    fixed install issue

--- webshot.rb
+++ webshot.rb
@@ -4,6 +4,10 @@ begin
   require 'pry'
   require 'thread/pool'
   require 'optparse'
+	require 'nmap/parser'
+	require 'nokogiri'
+	require 'net/http'
+	require 'openssl'
 rescue LoadError => msg
   puts msg.message.chomp + " try running \'bundle install\' first"
   exit!
@@ -27,6 +31,23 @@ args = OptionParser.new do |opts|
 end
 args.parse!(ARGV)
 
+@options[:urls] = Array.new
+puts "Extracting URLs from Nmap scan"
+xml = Nokogiri::XML(@options[:targets])
+xml.xpath("//host")[1..-1].each do |host|
+	ip = host.css('address').attr('addr').text
+	host.css("port").each do |p|
+		if p.css('state').attr('state').value == "open"
+			port = p.attr('portid')
+			proto = p.css('service').attr('name').text.split('-')[0]
+			if proto.include? 'http'
+				the_url = "#{proto}://#{ip}:#{port}"
+				@options[:urls] << the_url.chomp
+			end
+		end
+	end
+end
+
 if @options[:threads]
   if @options[:threads] > 20 || @options[:threads] < 1
     puts "Error: Thread count must be an integer value between 1 & 20"
@@ -37,8 +58,8 @@ if @options[:threads]
 else
   @threads = Thread.pool(10)
 end
-puts "Thread count set to #{@threads.max.to_s}." if @options[:verbose]
 
+puts "Configuring IMGKit options"
 IMGKit.configure do |config|
   config.default_options = {
     quality: 25,
@@ -52,42 +73,34 @@ IMGKit.configure do |config|
   }
 end
  
-def get_url(ip)
-  host = ip.split(":")[0]
-  port = ip.split(":")[1]
-  if port == "443"
-    "https://#{host}:#{port}/"
-  else
-    "http://#{host}:#{port}/"
-  end
-end
- 
 def get_screenshot(url)
-  begin
-    puts "Taking screenshot: #{url}" if @options[:verbose]
-    screenshot = IMGKit.new(url, quality: 25, height: 600, width: 800)
-  rescue => error
-    puts error
-    return nil
-  end
-  return screenshot
+	begin
+		puts "Taking screenshot: #{url}" if @options[:verbose]
+		screenshot = IMGKit.new(url, quality: 25, height: 600, width: 800)
+	rescue => error
+		puts error
+		return nil
+	end
+	return screenshot
 end
- 
-@options[:targets].each_line do |target|
+
+puts "Capturing #{@options[:urls].size.to_s} screenshots using #{@threads.max.to_s} threads"
+@options[:urls].each do |url|
   @threads.process {
-    url = get_url(target.chomp)
-    screenshot = get_screenshot(url)
-    if screenshot
-      begin
-        screenshot.to_file("#{@options[:output]}/#{target.chomp.gsub(/:/,'_')}.png")
-      rescue => error
-        if error.message.include? "No such file or directory"
-          puts "Directory \"#{@options[:output]}\" does not exist."
-          exit!
-        end
-        next
-      end
-    end
+		screenshot = get_screenshot(url)
+		if screenshot
+			begin
+				ip = url.split('//')[1]
+				file = screenshot.to_file("#{@options[:output]}/#{ip.gsub(/[:\/]/,'_')}.png")
+				FileUtils.rm(file.path) unless file.size > 3273
+			rescue => error
+				if error.message.include? "No such file or directory"
+					puts "Directory \"#{@options[:output]}\" does not exist."
+					return
+				end
+				next
+			end
+		end
   }
 end
 @threads.shutdown
